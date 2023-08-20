5c610bfe2fb3a4e6ff62ead12e4c1b4b81ea9790
Akashdeep-singh-4514 <waliaakash95@gmail.com>
2023-08-20 12:14:46 +0530

    validating email

--- Client/main.js
+++ Client/main.js
@@ -1,7 +1,11 @@
 
 
 // console.log("hello");
-
+function validateEmail(email) {
+    var regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    console.log(regex.test(email)+" email");
+    return regex.test(email);
+}
 document.getElementById('submit-btn').addEventListener('click', (e) => {
 
     e.preventDefault()
@@ -16,40 +20,43 @@ document.getElementById('submit-btn').addEventListener('click', (e) => {
     console.log(user_contact);
     console.log(user_email);
     console.log(user_password);
-    
 
-// ===================isko add event listner ke ander daal server on karna ===================================
-window.localStorage.setItem("myemail",user_email)
-//    ===============================================================
+    if (validateEmail(user_email) && user_contact != "" && user_fname != "" && user_lname != "" && user_password != "") {
+        window.localStorage.setItem("myemail", user_email)
 
-    user = {
-        fname: user_fname,
-        lname: user_lname,
-        contact: user_contact,
-        email: user_email,
-        password: user_password
-    }
-    console.log(user);
-
-    const xhr = new XMLHttpRequest()
-    url = `http://localhost:8082/signup`
-    xhr.open('POST', url)
-    xhr.setRequestHeader('Access-Control-Allow-Origin','*')
-    xhr.setRequestHeader('Content-Type', 'application/json')
-
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            res = JSON.parse(xhr.responseText)
-            console.log(res);
-
+        user = {
+            fname: user_fname,
+            lname: user_lname,
+            contact: user_contact,
+            email: user_email,
+            password: user_password
         }
+        console.log(user);
+
+        const xhr = new XMLHttpRequest()
+        url = `http://localhost:8082/signup`
+        xhr.open('POST', url)
+        xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
+        xhr.setRequestHeader('Content-Type', 'application/json')
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                res = JSON.parse(xhr.responseText)
+                console.log(res);
+
+                window.location.href = "http://127.0.0.1:5500/Client/home.html"
+            }
+        }
+
+        xhr.send(JSON.stringify(user))
+
+        
+
+
+
+    } else {
+        document.getElementById("message").innerHTML="put complete and correct details"
     }
-
-    xhr.send(JSON.stringify(user))
-
-    window.location.href="http://127.0.0.1:5500/home.html"
-
-
 })
 
 
